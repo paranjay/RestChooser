dbacc6de7861134b6bc8035de3c0694e51d9705b
paranjay <paranjay.pj@gmail.com>
2015-11-06 18:45:19 -0500

    filter activity working

--- app/src/main/java/edu/osu/restchooser/CreateFilters.java
+++ app/src/main/java/edu/osu/restchooser/CreateFilters.java
@@ -13,7 +13,6 @@ public class CreateFilters extends AppCompatActivity {
         setContentView(R.layout.activity_create_filters);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -23,12 +22,8 @@ public class CreateFilters extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
