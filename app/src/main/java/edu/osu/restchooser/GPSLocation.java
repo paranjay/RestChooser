2088f99f550dc922841f1462448538eb9fbbf922
alacto <alcoalco22@yahoo.com>
2015-11-09 10:00:53 -0500

    Add gps
    Adds gps class for location services

---
+++ app/src/main/java/edu/osu/restchooser/GPSLocation.java
@@ -0,0 +1,58 @@
+package edu.osu.restchooser;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by alcoa on 11/9/2015.
+ */
+public class GPSLocation extends Activity {
+
+    private LatLng gpsLocation;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new myLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    class myLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null){
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                gpsLocation = new LatLng(latitude, longitude);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+
+
+}
